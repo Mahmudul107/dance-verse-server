b62bbca98fb5f37167a3b914c159947569cdc321
Mahmudul107 <mahmudulislam378@gmail.com>
2023-06-12 21:29:19 +0600

    made an admin with patch

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb"); // mongodb
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// const { MongoClient, ServerApiVersion } = require("mongodb");    
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.py1cfi4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,23 +26,45 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
     // Collections
-    const usersCollection = client.db('danceDB').collection('users');
-
-
+    const usersCollection = client.db("danceDB").collection("users");
 
     // users collection apis
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-    })
+    app.get("/users", async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    });
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      // const query = {email: user.email}
+      // const existingUser = await usersCollection.findOne(query);
+      // if (existingUser){
+      //     return res.send({message: 'User already exists'})
+      // }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Make admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: `admin`,
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -52,13 +72,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Website is dancing");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send('Website is dancing');
-})
-
-
-app.listen( port, () =>{
-    console.log(`Dance verse is already listening on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Dance verse is already listening on port ${port}`);
+});
