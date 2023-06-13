5ee11866d86d1920dd127f14ec032d0f0090ca0b
Mahmudul107 <mahmudulislam378@gmail.com>
2023-06-13 10:28:14 +0600

    admin security check completed

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  //get bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+
 // const { MongoClient, ServerApiVersion } = require("mongodb");    
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.py1cfi4.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -55,6 +74,23 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+
+
+    // first security check
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.body.email;
+
+      if(req.decoded.email !== email) {
+        res.send({admin: false})
+      }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      const result = {admin: user.role === 'admin'}
+      res.send(result)
+    })
+
 
     // Make admin
     app.patch("/users/admin/:id", async (req, res) => {
